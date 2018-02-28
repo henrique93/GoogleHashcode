d3495376ef50c75f3c3c46c4009ce49b6febf3c6
migueltouret <miguel_touret@hotmail.com>
2018-02-28 19:52:38 +0000

    lixo

--- NotAux.py
+++ NotAux.py
@@ -19,3 +19,28 @@ def fileReader(f):
         #print(l)
     #-------------------------------------------------------------------
     return (rows, columns, minIngredient, maxIngredient, matrix)
+
+def exampleSubmition(f):
+    values = filereader(f)
+    rows = values[0]
+    columns = values[1]
+    minIngredient = values[2]
+    maxIngredient = values[3]
+    matrix = values[4]
+    for l in range(0, rows):
+        for c in range(0 , collumns):
+            print(lul)
+            
+            
+def slice(currentRow,currentColumn,maxRow,maxColumn,minIngredient,maxIngredient,matrix):
+    if(( currentRow + maxIngredient) > maxRow):
+        maxRowSlice = maxRow
+    else: 
+        maxRowSlice = currentRow + maxIngredient
+    if(( currentColumn + maxIngredient) > maxColumn):
+        maxColumnSlice = maxColumn
+    else: 
+        maxColumnSlice = currentColumn + maxIngredient    
+    for l in range(0, maxRowSlice - currentRow) :
+        for c in range(0, maxColumnSlice - currentColumn):
+            
