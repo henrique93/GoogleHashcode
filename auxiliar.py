194d2659d4ae9c32dd7ec53653651407ca865e95
Henrique Afonso Ferreira da Silva Lourenço <Henrique@MacBook-Pro-de-Henrique.local>
2018-03-01 21:07:55 +0000

    done2

--- auxiliar.py
+++ auxiliar.py
@@ -1,12 +1,9 @@
 def fileReader(f):
     rides = []
-    vehicles = []
     j = 0
     with open(f, 'r') as file:
         line = file.readline()
         rows, columns, vehicleNum, rideNum, bonus, steps = [int(val) for val in line.split()]
-        for i in range(0, vehicleNum):
-            vehicles.append([(0,0), 0])
         for line in file:
             line = line.split()
             #ride [ride number, start coordinates, finish coordinates, earliest start, latest finish]
@@ -24,7 +21,7 @@ def fileReader(f):
     #for l in rides:
         #print(l)
     #-------------------------------------------------------------------
-    return (rows, columns, vehicles, rideNum, bonus, steps, rides)
+    return (rows, columns, vehicleNum, rideNum, bonus, steps, rides)
 
 def assignRide(vehiclePos, steps, rides):
     for ride in rides:
@@ -55,19 +52,25 @@ def assignRide(vehiclePos, steps, rides):
 def main(inputFile):
     step = 0
     result = []
-    rows, columns, vehicles, rideNum, bonus, maxSteps, rides = fileReader(inputFile)
-    for i in range(len(vehicles)):
+    vehicles = []
+    rows, columns, vehicleNum, rideNum, bonus, maxSteps, rides = fileReader(inputFile)
+    for i in range(0, vehicleNum):
+        vehicles.append([(0,0), 0])
+    for i in range(vehicleNum):
         result.append([0])
-    while ((len(rides) > 0) and (step < maxSteps)): #FIXME
+    moved = True
+    while ((len(rides) > 0) and (step < maxSteps) and (moved)): #FIXME
+        moved = False
         nextStep = maxSteps
         for vehicle in vehicles:
             if (vehicle[1] < nextStep):
                 nextStep = vehicle[1]
         step = nextStep
-        for v in range(len(vehicles)):
+        for v in range(vehicleNum):
             if (vehicles[v][1] == step):
                 ride, rides, distFinish = assignRide(vehicles[v][0], step, rides)
                 if (ride is not None):
+                    moved = True
                     vehicles[v] = [ride[2], distFinish]
                     result[v][0] += 1
                     result[v].append(ride[0])
